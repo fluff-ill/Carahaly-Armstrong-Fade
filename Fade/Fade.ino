ccf191d818e6bf5befa8754943b18b0751b99bcc
Sophia Carahaly <02sophia.carahaly@gulllakecs.org>
2019-04-22 09:45:12 -0400

    changed brightness to 2

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -14,7 +14,7 @@
 */
 
 int led = 9;           // the PWM pin the LED is attached to
-int brightness = 0;    // how bright the LED is
+int brightness = 2;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
