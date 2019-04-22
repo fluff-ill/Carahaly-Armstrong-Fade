6b3406d950dfae2da45ed1bdc21313eca4193755
Noah Armstrong <noah.armstrong@gulllakecs.org>
2019-04-22 06:45:47 -0700

    changed fadeAmount

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -15,7 +15,7 @@
 
 int led = 9;           // the PWM pin the LED is attached to
 int brightness = 0;    // how bright the LED is
-int fadeAmount = 5;    // how many points to fade the LED by
+int fadeAmount = 6;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
